d3693ef0f33d9b4216cf0a09c0ab9c82a68e7b7a
Kevin <ktripp1@gmail.com>
2013-11-13 08:31:22 -0500

    fixed the bug where the progress bars would animate multiple times

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,19 +3,27 @@
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 $(document).ready(function(){
+
 	$('.college_submit').hide();
+
+
 
 	$("[name='college[accepted]']").click(function(){
 		$(this).closest('form').submit();
 	});
 
+
+var runProgressBars = function(){
 	for (i = 0; i < gon.colleges.length; i++){
 		console.log(gon.colleges[i].name)
 		console.log(gon.colleges[i].percent_complete)
-		for (j = 0; j <= gon.colleges[i].percent_complete; j++){
-			$('.' + gon.colleges[i].name).animate({width:j+'%'},50,function(){
+			$('.progress').css('width','0');
+			$('.' + gon.colleges[i].name).animate({width:gon.colleges[i].percent_complete + '%'},5000,function(){
 				//complete
 			});
-		}
 	}
+}
+
+runProgressBars();
+
 });
